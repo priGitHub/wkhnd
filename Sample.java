96e7385211ad3e269965085f0f7361bed6e08d0e
priGitHub <priyanarayananid@gmail.com>
2020-03-09 18:48:23 +0000

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,9 @@ class Sample{
 public Sample(){
 	int count =5;
 }
+priavte static String SimpleOut(){
+	return "rose";
+}
 public static void main(String a[]){
 		System.out.println("inside SOP of mian");
 		Sample sam=new Sample();
