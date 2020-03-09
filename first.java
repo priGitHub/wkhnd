231ebff194f52087bd069417346c0c8fbbf2ee58
priGitHub <priyanarayananid@gmail.com>
2020-03-09 09:23:23 +0000

    Update first.java

--- first.java
+++ first.java
@@ -1,4 +1,8 @@
 class first{
+ public first(){
+   int count = 10;
+   System.out.println("I am inside constructor");
+ }
 public static void main(String a[]){
   System.out.println("this is a simple program in java");
 }
