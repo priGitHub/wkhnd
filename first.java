53edc4bede214f5366f8f46263c5da1cefbbf49a
priGitHub <priyanarayananid@gmail.com>
2020-03-09 09:28:27 +0000

    Update first.java

--- first.java
+++ first.java
@@ -3,6 +3,9 @@ class first{
    int count = 10;
    System.out.println("I am inside constructor");
  }
+ private int sum(int a, int b){
+  return a+b;
+ }
 public static void main(String a[]){
   System.out.println("this is a simple program in java");
  First fst=new First();
